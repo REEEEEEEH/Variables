38fa72072de6a9bd4a9b12097695f6a7eef9686d
Vicen <85416824+Vicensk@users.noreply.github.com>
2021-06-17 20:22:51 -0700

    when java is sus

---
+++ src/main.java
@@ -0,0 +1,24 @@
+package packageLOL;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vicensk the idiot coded this");
+		
+		short sum;
+		sum = 3+20;
+		int sub = 8 -17;
+		long residue = 9/2;
+		float multiply = 2*(15* (-2));
+		double division = 10/3.4;
+		
+		System.out.println("The result of the sum is: "+sum);
+		System.out.println("The result of the subtract is: "+sub);
+		System.out.println("The result of the multiply is: "+multiply);
+		System.out.println("The result of the division is: "+division);
+		System.out.println("The residue is: "+residue);
+
+	}
+
+}
